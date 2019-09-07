0986d936678b3b0923314d0b70e2d8c4820d8f08
Iamelvira <elyakraso4naja@gmail.com>
2019-09-07 12:44:01 +0300

    1

---
+++ ControlWork2/Files.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlWork2
+{
+    public abstract class File
+    {
+        
+        public string Name { get; set; }
+        public string Extension { get; set; }
+        public string Size { get; set; }
+        public virtual void Parce(string input)
+        {
+            string[] splitInput = input.Split(new char[] { '(', ')', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            string nameWithExt = splitInput[0];
+            Size = splitInput[1];
+        }
+
+    }
+    public class TextFile : File
+    {
+        public string Content { get; set; }
+        public string Count { get; set; }
+        public TextFiles(string[] strarr) : base(strarr)
+        {
+            string[] filecontent = strarr[4].Split(';');
+            Content = strarr[4];
+
+        }
+
+    }
+    public class ImageFile : File
+    {
+        public string Content { get; set; }
+        public static int Count { get; set; }
+        public ImageFiles(string[] strarr) : base(strarr)
+        {
+            string[] filecontent = strarr[4].Split(';');
+            Content = strarr[4];
+
+        }
+    }
+    public class VideoFile : Files
+    {
+        public string Length { get; set; }
+        public string Resolution
+        {
+            get: set; }
+        }
+    }
+}
--- ControlWork2/Program.cs
+++ ControlWork2/Program.cs
@@ -4,12 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ControlWork2
-{
+using System;using System.Collections.Generic;using System.Linq;using System.Text;using System.Threading.Tasks;namespace ControlWork2{
     class Program
     {
         static void Main(string[] args)
         {
+            string text = @"Text:file.txt(6B);Some string content							Image:img.bmp(19MB);1920х1080							Text:data.txt(12B);Another string							Text:data1.txt(7B);Yet another string							Movie:logan.2017.mkv(19GB);1920х1080;2h12m";
+
+            List<File> result = new List<File>();
+            string[] files = text.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < files.Length; i++)
+            {
+                string[] split = files[i].Split(':');
+                string fileType = files[i].Split(':')[0].ToLower();
+                File newFile = null;
+                switch (fileType)
+                {
+                    case "text":
+                        //string[] splitFileInfo = split[1].Split(new char[] { '(',')', ';' }, StringSplitOptions.RemoveEmptyEntries);
+                        TextFile textFile = new TextFile();
+                        textFile.Parce(split[1]);
+                        result.Add(textFile);
+                        break;
+                    case "image":
+                        ImageFile imageFile = new ImageFile();
+                        imageFile.Parce(split[1]);
+                        result.Add(imageFile);
+                        break;
+                    case "movie":
+                        VideoFile movieFile = new VideoFile();
+                        movieFile.Parce(split[1]);
+                        result.Add(movieFile);
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
-    }
-}
+    }}}
